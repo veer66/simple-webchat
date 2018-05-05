4519c1fafccbe9f25a8db906987c9fd4c4f3bb4b
Vee Satayamas <vsatayamas@gmail.com>
2018-05-05 12:39:23 +0700

    start

---
+++ src/main/java/rockers/veer66/App.java
@@ -0,0 +1,13 @@
+package rockers.veer66;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
